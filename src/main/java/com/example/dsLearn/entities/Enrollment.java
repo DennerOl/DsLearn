1f363483b1df32c407aeeaee0cfef6bfd11cbf9c
DennerOl <dener155@hotmail.com>
2024-01-31 21:45:09 -0300

    Seed Lesson e ajustes

--- src/main/java/com/example/dsLearn/entities/Enrollment.java
+++ src/main/java/com/example/dsLearn/entities/Enrollment.java
@@ -1,12 +1,15 @@
 package com.example.dsLearn.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.example.dsLearn.entities.pk.EnrollmentPK;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class Enrollment {
   private Instant refundMoment;
   private boolean available;
   private boolean onlyUpdate;
+
+  @ManyToMany(mappedBy = "enrollmentsDone")
+  private Set<Lesson> lessonsDone = new HashSet<>();
 
   public Enrollment() {
   }
